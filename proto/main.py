c8914233e7c03a7e60ea9608f30d6706a07169a2
josef.spjut <josef.spjut@6f93ab30-5ef1-11de-8832-3f4c773c73ae>
2011-04-12 19:43:19 +0000

    Currently functions in a confetti mode. Stars dissappear when they go off the screen but a new set of randomly colored ones is created with every left click.

--- proto/main.py
+++ proto/main.py
@@ -12,8 +12,15 @@ window = pyglet.window.Window(resizable=True)
 
 center = [window.width/2, window.height/2]
 ps = []
-for i in xrange(int(window.height/10)):
-    ps.append(particle.Particle(center, [random.randrange(-200, 200),random.randrange(-200,200)]))
+
+def fireworks(pos, num):
+    for i in xrange(num):
+        color = (random.randrange(255), random.randrange(255), random.randrange(255))
+        ps.append(particle.Particle(pos, [random.randrange(-200, 200),random.randrange(-200,200)], color))
+
+fireworks(center, int(window.height/10))
+# for i in xrange(int(window.height/10)):
+#     ps.append(particle.Particle(center, [random.randrange(-200, 200),random.randrange(-200,200)]))
 
 wrap = False
 
@@ -21,20 +28,24 @@ wrap = False
 def on_draw():
     window.clear()
     for p in ps:
-        p.draw()
+        vlist = p.get_vertex_list()
+        vlist.draw(p.get_vertex_type())
 
 def update(dt):
     for p in ps:
         p.update(dt)
-        if wrap:
-            p.wrap_around(window.width, window.height)
-        else:
-            p.bounce_around(window.width, window.height)
+        if p.is_dead(window.width, window.height):
+            ps.remove(p)
+#         if wrap:
+#             p.wrap_around(window.width, window.height)
+#         else:
+#             p.bounce_around(window.width, window.height)
 pyglet.clock.schedule(update)
+
 
 @window.event
 def on_mouse_press(x, y, button, modifiers):
-    if button == mouse.LEFT:
+    if button == mouse.MIDDLE:
         for p in ps:
             p.pos[0] = x
             p.pos[1] = y
@@ -42,5 +53,9 @@ def on_mouse_press(x, y, button, modifiers):
         for p in ps:
             p.vel[0] = x - p.pos[0]
             p.vel[1] = y - p.pos[1]
+    if button == mouse.LEFT:
+        # Fireworks!
+        center = [x,y]
+        fireworks(center, 17)
 
 pyglet.app.run()
--- proto/particle.py
+++ proto/particle.py
@@ -1,29 +1,31 @@
 import pyglet
 
 class Particle:
-    def __init__(self, pos, vel):
+    def __init__(self, pos, vel, color = (0, 0, 255)):
         self.pos = [pos[0], pos[1]]
         self.vel = vel
-        self.color = (0,0,255)
+        self.color = color
 
-    def draw(self):
+    def get_vertex_type(self):
+        return pyglet.gl.GL_TRIANGLE_FAN
+
+    def get_vertex_list(self):
         size = 5
-        pyglet.graphics.draw(6, pyglet.gl.GL_TRIANGLE_FAN,
-                             ('v2f', (self.pos[0], self.pos[1],
-                                      self.pos[0] + size, self.pos[1] + 0,
-                                      self.pos[0] + 0, self.pos[1] + size,
-                                      self.pos[0] + -size, self.pos[1] + 0,
-                                      self.pos[0] + 0, self.pos[1] + -size,
-                                      self.pos[0] + size, self.pos[1] + 0
-                                      )),
-                             ('c3B', (255, 255, 255,
-                                      0, 0, 255,
-                                      0, 0, 255,
-                                      0, 0, 255,
-                                      0, 0, 255,
-                                      0, 0, 255))
-                             )
-        pass
+        return pyglet.graphics.vertex_list(6,
+                  ('v2f', (self.pos[0], self.pos[1],
+                           self.pos[0] + size, self.pos[1] + 0,
+                           self.pos[0] + 0, self.pos[1] + size,
+                           self.pos[0] + -size, self.pos[1] + 0,
+                           self.pos[0] + 0, self.pos[1] + -size,
+                           self.pos[0] + size, self.pos[1] + 0
+                           )),
+                  ('c3B', (255, 255, 255,
+                           self.color[0], self.color[1], self.color[2],
+                           self.color[0], self.color[1], self.color[2],
+                           self.color[0], self.color[1], self.color[2],
+                           self.color[0], self.color[1], self.color[2],
+                           self.color[0], self.color[1], self.color[2]))
+                  )
 
     def update(self, dt):
         self.pos[0] = self.pos[0] + self.vel[0] * dt
@@ -44,3 +46,8 @@ class Particle:
             self.vel[0] = -self.vel[0]
         if (self.pos[1] > height and self.vel[1] > 0) or (self.pos[1] < 0 and self.vel[1] < 0):
             self.vel[1] = -self.vel[1]
+
+    def is_dead(self, width, height):
+        if (self.pos[0] > width or self.pos[1] > height or self.pos[0] < 0 or self.pos[1] < 0):
+            return True
+        return False
